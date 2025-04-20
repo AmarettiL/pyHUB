e413e2f2cd9c0bf834d33392b8d3806b568393ac
Luca Amaretti <81777211+AmarettiL@users.noreply.github.com>
2025-04-20 14:51:55 +0200

    Update order.py

--- order.py
+++ order.py
@@ -332,4 +332,5 @@ def reset_counter():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Usa la porta specificata da Render o 5000 come fallback
+    app.run(host='0.0.0.0', port=port, debug=True)
